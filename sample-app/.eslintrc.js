42b4b305fc906294fb82ff66079f7fc93cb2165b
Koichi.Shimomura <unieye51@gmail.com>
2021-04-22 13:48:43 +0900

    2-4: VSCodeの設定

--- sample-app/.eslintrc.js
+++ sample-app/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
@@ -14,8 +14,8 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   overrides: [
     {
