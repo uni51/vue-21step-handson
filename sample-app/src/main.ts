434f7d1b0fa4288e9dd710f32090de6351b68cc5
Koichi.Shimomura <unieye51@gmail.com>
2021-04-26 18:06:35 +0900

    fix: 2-6 ver.2fix: 2-6 ver.2fix: 2-6 ver.2fix: 2-6 ver.2fix: 2-6 ver.2fix: 2-6 ver.2

--- sample-app/src/main.ts
+++ sample-app/src/main.ts
@@ -1,7 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import VueCompositionAPI from '@vue/composition-api';
 Vue.use(VueCompositionAPI);
 
@@ -10,5 +10,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
