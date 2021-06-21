b43b6bec00df078ceaf02907396ce3dd665e2b67
Koichi.Shimomura <unieye51@gmail.com>
2021-06-21 16:27:12 +0900

    Step11: ライフサイクルフックを使ってみよう

--- sample-app-js/src/views/Calendar.vue
+++ sample-app-js/src/views/Calendar.vue
@@ -285,6 +285,7 @@ import {
   computed,
   nextTick,
   toRefs,
+  onUpdated,
 } from '@vue/composition-api';
 import {
   parseDate,
@@ -389,6 +390,18 @@ export default defineComponent({
       disabledEndTime: computed(() => {
         return !state.endDate || !state.startTime;
       }),
+    });
+
+    /**
+     * `onUpdated`ライフサイクルフックです。
+     */
+    onUpdated(() => {
+      if (props.type === 'month') {
+        return;
+      }
+      nextTick(() => {
+        state.calendar.scrollToTime('07:00');
+      });
     });
 
     const methods = {
