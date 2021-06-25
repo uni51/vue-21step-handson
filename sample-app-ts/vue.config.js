1d3f4ec231c26a145523e3281a2b74dc3ce618bc
Koichi.Shimomura <unieye51@gmail.com>
2021-06-25 09:12:54 +0900

    13-1: WebAPIをモック化しよう

--- sample-app-ts/vue.config.js
+++ sample-app-ts/vue.config.js
@@ -1,7 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const path = require('path');
+const apiMocker = require('mocker-api');
 
 module.exports = {
+  devServer: {
+    before(app) {
+      apiMocker(app, path.resolve('@/../mock/api-mock.js'), {
+        proxy: {
+          '/api/(.*)': 'http://localhost:8080/',
+        },
+        changeHost: true,
+      });
+    },
+  },
   transpileDependencies: ['vuetify'],
   configureWebpack: {
     plugins: [
