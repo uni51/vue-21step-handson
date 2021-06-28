408ce86267d01840a57f865af9e1785047733b3c
Koichi.Shimomura <unieye51@gmail.com>
2021-06-28 14:35:13 +0900

    13-4: WebAPIでプロフィールを更新する

--- sample-app-js/src/store/profile.js
+++ sample-app-js/src/store/profile.js
@@ -1,16 +1,26 @@
 import '@/plugins/composition-api';
 import { reactive } from '@vue/composition-api';
+import axios from 'axios';
 import { update } from '@/store/shared-user';
 
 export const profileStore = reactive({ profile: null });
 
 /**
- * ユーザー名を更新します
+ * ユーザー名を更新します。
  * @param userName ユーザー名
  */
-export const updateUserName = userName => {
-  profileStore.profile.userName = userName;
-  update(profileStore.profile);
+export const updateUserNameAsync = async userName => {
+  const profile = profileStore.profile;
+  const data = { userName: userName };
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    await axios.patch('profile', data);
+    profile.userName = userName;
+    update(profile);
+  } catch (error) {
+    throw error;
+  }
 };
 
 /**
@@ -29,4 +39,19 @@ export const updateNickname = nickname => {
 export const updateThemeColor = themeColor => {
   profileStore.profile.themeColor = themeColor;
   update(profileStore.profile);
+};
+
+/**
+ * サインインします。
+ */
+export const signInAsync = async () => {
+  // ここに外部認証に関連した実装が必要
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const response = await axios.get('profile');
+    profileStore.profile = response.data;
+  } catch (error) {
+    throw error;
+  }
 };
--- sample-app-ts/src/store/profile.ts
+++ sample-app-ts/src/store/profile.ts
@@ -1,19 +1,28 @@
 import '@/plugins/composition-api';
 import { reactive } from '@vue/composition-api';
+import axios from 'axios';
 import { Profile } from '@/store/profile.model';
 import { update } from '@/store/shared-user';
 
 export const profileStore = reactive({ profile: null as Profile | null });
 
 /**
- * ユーザー名を更新します
+ * ユーザー名を更新します。
  * @param userName ユーザー名
  */
-export const updateUserName = (userName: string) => {
+export const updateUserNameAsync = async (userName: string) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  profileStore.profile!.userName = userName;
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  update(profileStore.profile!);
+  const profile = profileStore.profile!;
+  const data = { userName: userName };
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    await axios.patch('profile', data);
+    profile.userName = userName;
+    update(profile);
+  } catch (error) {
+    throw error;
+  }
 };
 
 /**
@@ -36,4 +45,19 @@ export const updateThemeColor = (themeColor: string) => {
   profileStore.profile!.themeColor = themeColor;
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   update(profileStore.profile!);
+};
+
+/**
+ * サインインします。
+ */
+export const signInAsync = async () => {
+  // ここに外部認証に関連した実装が必要
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const response = await axios.get<Profile>('profile');
+    profileStore.profile = response.data;
+  } catch (error) {
+    throw error;
+  }
 };
