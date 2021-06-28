8309feb680d5b9239e9e03230ddfa1d76e03b125
Koichi.Shimomura <unieye51@gmail.com>
2021-06-28 17:52:10 +0900

    15-4: Vuexのストアを作成する

--- sample-app-js/src/store/index.js
+++ sample-app-js/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { profileModule } from '@/store/profile/profile';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    profile: profileModule,
+  },
 });
