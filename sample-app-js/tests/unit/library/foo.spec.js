fef0fd3c544a37bc01e6a2e01c9bd23f477ebda1
Koichi.Shimomura <unieye51@gmail.com>
2021-07-07 10:32:27 +0900

    20.1: モジュールのモック化 4.モック化処理を共通化する

--- sample-app-js/tests/unit/library/foo.spec.js
+++ sample-app-js/tests/unit/library/foo.spec.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-undef */
 // テスト対象の関数
 import { foo } from '@/library/foo';
-// モック化対象のオブジェクト
-import * as veeValidate from 'vee-validate';
-
-// vee-validateをモック化
-jest.spyOn(veeValidate, 'validate');
 
 // foo 関数のテスト
 describe('foo', () => {
+  // テスト実行前にモック化を解除
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   // foo 関数が true を返したときの挙動を確認するテスト
   it('function foo (validate) return true', async () => {
     // validate が必ず true を返すようにモック化
-    veeValidate.validate.mockResolvedValue({ valid: true });
+    mockVeeValidate(true);
     // テスト対象関数を実行
     const result = await foo('123');
     // 戻り値が true であることを検証
@@ -21,7 +22,7 @@ describe('foo', () => {
   // foo 関数が false を返したときの挙動を確認するテスト
   it('function foo (validate) return false', async () => {
     // validate が必ず false を返すようにモック化
-    veeValidate.validate.mockResolvedValue({ valid: false });
+    mockVeeValidate(false);
     // テスト対象関数を実行
     const result = await foo('123');
     // 戻り値が false であることを検証
--- sample-app-ts/tests/unit/library/foo.spec.ts
+++ sample-app-ts/tests/unit/library/foo.spec.ts
@@ -1,18 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // テスト対象の関数
 import { foo } from '@/library/foo';
-// モック化対象のオブジェクト
-import * as veeValidate from 'vee-validate';
-
-// vee-validateをモック化
-jest.spyOn(veeValidate, 'validate');
+declare let mockVeeValidate: any;
 
 // foo 関数のテスト
 describe('foo', () => {
+  // テスト実行前にモック化を解除
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   // foo 関数が true を返したときの挙動を確認するテスト
   it('function foo (validate) return true', async () => {
     // validate が必ず true を返すようにモック化
-    (veeValidate.validate as any).mockResolvedValue({ valid: true });
+    mockVeeValidate(true);
     // テスト対象関数を実行
     const result = await foo('123');
     // 戻り値が true であることを検証
@@ -22,7 +23,7 @@ describe('foo', () => {
   // foo 関数が false を返したときの挙動を確認するテスト
   it('function foo (validate) return false', async () => {
     // validate が必ず false を返すようにモック化
-    (veeValidate.validate as any).mockResolvedValue({ valid: false });
+    mockVeeValidate(false);
     // テスト対象関数を実行
     const result = await foo('123');
     // 戻り値が false であることを検証
