51bff03c634e27ce72cf47690ac07716734c7c7b
Koichi.Shimomura <unieye51@gmail.com>
2021-07-08 09:50:18 +0900

    20.2 ストアの単体テスト

--- sample-app-ts/tests/unit/jest-setup.ts
+++ sample-app-ts/tests/unit/jest-setup.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // モック化対象のオブジェクト
 import * as veeValidate from 'vee-validate';
+// モック化対象のオブジェクト
+import axios from 'axios';
 declare let global: any;
 
 global.mockVeeValidate = (valid: boolean) => {
@@ -8,4 +10,14 @@ global.mockVeeValidate = (valid: boolean) => {
   jest
     .spyOn(veeValidate, 'validate')
     .mockResolvedValue({ valid: valid } as any);
+};
+
+global.mockAxiosGet = (value: any) => {
+  // axios.get をモック化
+  jest.spyOn(axios, 'get').mockResolvedValue(value);
+};
+
+global.mockAxiosPatch = (value: any) => {
+  // axios.patch をモック化
+  jest.spyOn(axios, 'patch').mockResolvedValue(value);
 };
